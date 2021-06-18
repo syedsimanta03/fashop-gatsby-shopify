4a832ebba8f7f684970364e69753a5a67105d80e
sam <syed.simanta06@gmail.com>
2021-06-19 00:03:26 +0600

    'shopifyGatsbyjs'

---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+  <a href="https://www.shopify.com">
+    <img alt="Shopify" src="https://cdn.shopify.com/shopifycloud/web/assets/v1/2217fb04df073033ccce8d125b0ea020.svg" width="60" />
+  </a>
+</p>
+<h1 align="center">
+  Gatsby Theme Development Shopify
+</h1>
+
+
+## 🚀 Quick start
+
+
+ **Start server.**
+
+    Navigate into your new site’s directory and start it up.
+
+    ```shell
+    cd ./
+    gatsby develop
+    ```
+
+4.  **Open the source code and start editing!**
+
+    Your site is now running at `http://localhost:8000`!
+
+    _Note: You'll also see a second link: _`http://localhost:8000/___graphql`_. This is a tool you can use to experiment with querying your data. 
+
---
+++ src/components/footer.jsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import Logo from "../icons/logo"
+import {
+  footerStyle,
+  copyright,
+  links,
+  blurb,
+  logos,
+  footerNavList,
+  footerNavListItem,
+} from "./footer.module.css"
+
+export function Footer() {
+  return (
+    <footer className={footerStyle}>
+      <div className={blurb}>
+        <div className={logos}>
+          <Logo />
+        </div>
+      </div>
+      <nav className={links} aria-label="footer">
+        <ul className={footerNavList}>
+          <li className={footerNavListItem}>
+            <p>Menu Items</p>
+          </li>
+        </ul>
+      </nav>
+      <div className={copyright}>
+        Copyright &copy; {new Date().getFullYear()} · All rights reserved
+      </div>
+    </footer>
+  )
+}
